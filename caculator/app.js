b149c0db33109e5cd5b5332bfb3f6a26a91a2aa0
yboCrafts <ytimola123@gmail.com>
2022-09-29 13:56:03 +0800

    Initial funcitonality for click

--- caculator/app.js
+++ caculator/app.js
@@ -7,13 +7,41 @@ window.addEventListener('keydown', e => {
     const key = e.key.toLocaleLowerCase();
 
     if(key == 'enter') {
-        console.log(`${value} = ${eval(value)}`);
+        document.querySelector('#formula').innerHTML = `${value}`;
+        document.querySelector('#sum').innerHTML = eval(value);
         value = '';
     }else {
         array.forEach(data => {
             if(data == key) {
                 value += e.key;
+                document.querySelector('#formula').innerHTML = '';
+                document.querySelector('#sum').innerHTML = value;
+
             }
         });        
     }
+});
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', ev => {
+        const key = ev.target.dataset.key;
+        console.log(key);
+
+        if(key == 'enter') {
+            document.querySelector('#formula').innerHTML = `${value}`;
+            document.querySelector('#sum').innerHTML = eval(value);
+            value = '';
+        }else {
+            array.forEach(data => {
+                if(data == key) {
+                    value += key;
+                    document.querySelector('#formula').innerHTML = '';
+                    document.querySelector('#sum').innerHTML = value;
+    
+                }
+            });        
+        }
+    }); 
 });
